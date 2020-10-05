472ef062c99e28b3f6f1e7fc960d9695d689c4d5
adnanshibli <adnannajem.24.95@gmail.com>
2020-10-05 15:34:12 +0300

    new java

---
+++ js/app.js
@@ -0,0 +1,65 @@
+'use strict';
+alert('Hello, we are now going on a guess game to know about me');
+var Name = prompt("Type in your name");
+alert("Hello " + Name);
+alert('now we will start our game ' + Name +' please answer by yes or no')
+var study=prompt( 'am I a civil engineer?');
+console.log(study);
+var study2 = study.toLowerCase();
+console.log(study2);
+
+ if(study2=='yes' || study=='y' || study=='Y'){
+      alert('your answer is correct');
+  }else{
+      alert('your answer wrong');
+  }
+var sport=prompt( 'do I like swimming?');
+console.log(sport);
+var sport2 = sport.toLowerCase();
+console.log(sport2);
+
+ if(sport2=='yes'){
+      alert('your answer is wrong');
+  }else if (sport2=="no" || sport=='n' || sport=='N'){
+      alert('your answer correct');
+  }else{
+      alert('your answer is wrong');
+  }
+  
+var food=prompt( 'is mansaf the favourite food for me?');
+console.log(food);
+var food2 = food.toLowerCase();
+console.log(food2);
+
+ if(food2=='yes' || food=='Y' || food=='y'){
+      alert('your answer is correct');
+  }else{
+      alert('your answer wrong');
+  }
+  
+  
+var mang=prompt( 'do I like managers?');
+console.log(mang);
+var mang2 = mang.toLowerCase();
+console.log(mang2);
+
+ if(mang2=='yes'){
+      alert('your answer is wrong');
+  }else if(mang2=='no' || mang=='n' || mang=='N'){
+      alert('your answer correct');
+  }else{
+      alert('your answer is wrong');
+  }
+  
+
+var games=prompt( 'do I like watch football games?');
+console.log(games);
+var games2 = games.toLowerCase();
+console.log(games2);
+
+ if(games2=='yes' || games=='Y' ||games=='y'){
+      alert('your answer is correct');
+  }else{
+      alert('your answer wrong');
+  }
+  
